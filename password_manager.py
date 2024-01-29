f8c1c230ad42af301a126b7590fb987eaa943a64
JonathanMPeres <emailmarceloperes@gmail.com>
2024-01-29 09:17:55 -0300

    function add implemented

--- password_manager.py
+++ password_manager.py
@@ -1,5 +1,11 @@
 master_pwd = input("What is the master password? ")
 
+def add():
+    name = input("Account Name: ")
+    pwd = input("Password: ")
+
+    with open("password.txt", "a") as f:
+        f.write(name + "|" + pwd + "\n")
 
 while True:
     mode = input("Would you like to add a new password or view existing ones (add, view)? Press 'q' to quit. ").lower()
